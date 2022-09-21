385fc5d88cc787b48d725a5ae9228bba96e7183d
최태규 <suker800@gmail.com>
2022-09-21 13:18:08 +0900

    요구사항 7.2.4 8. 하나의 JdbcTemplate의 PreparedStatementSetter , RowMapper로 분리

--- src/main/java/next/dao/PreparedStatementSetter.java
+++ src/main/java/next/dao/PreparedStatementSetter.java
@@ -3,6 +3,7 @@ package next.dao;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+@FunctionalInterface
 public interface PreparedStatementSetter {
     void setValues(PreparedStatement preparedStatement) throws SQLException;
 }
--- src/main/java/next/dao/RowMapper.java
+++ src/main/java/next/dao/RowMapper.java
@@ -3,6 +3,7 @@ package next.dao;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+@FunctionalInterface
 public interface RowMapper {
     Object mapRow(ResultSet resultSet) throws SQLException;
 }
